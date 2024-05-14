25a4c99bf1df68911b06985b1e5be9a1dc15e843
martytrajkoski <martytrajkoski@gmail.com>
2024-05-14 19:37:30 +0200

    completeApp

---
+++ app/(auth)/sign-up.jsx
@@ -0,0 +1,85 @@
+import { View, Text, Image, ScrollView, SafeAreaView, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { Link, router } from 'expo-router';
+import { images } from '../../constants';
+import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
+import { createUser } from '../../lib/appwrite';
+import { useGlobalContext } from '../../context/GlobalProvider';
+
+
+const SignUp = () => {
+  const { setuser, setisLoggedIn } = useGlobalContext();
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = async () => {
+    if(!form.username || !form.email || !form.password){
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+
+    setIsSubmitting(true)
+
+    try{
+      const result = await createUser(form.email, form.password, form.username)
+
+      setuser(result);
+      setisLoggedIn(true);
+
+      router.replace('/home')
+    } catch(error){
+      Alert.alert('Error', error.message)
+    } finally{
+      setIsSubmitting(false)
+    }
+
+  }
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]"/>
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Sign up to Aora</Text>
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) =>setForm({...form, username:e})}
+            otherStyles="mt-10"
+          />
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) =>setForm({...form, email:e})}
+            otherStyles="mt-7"
+            keyboardType="email-address" //for autofill information
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) =>setForm({...form, password:e})}
+            otherStyles="mt-7"
+          />
+          <CustomButton 
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">Have an account already?</Text>
+            <Link href='/sign-in' className='text-lg font-psemibold text-secondary'>Sign In</Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default SignUp
---
+++ context/GlobalProvider.js
@@ -0,0 +1,47 @@
+import { Children, createContext, useContext, useEffect, useState } from "react";
+import { getCurrentUser } from "../lib/appwrite";
+
+const GlobalContext = createContext();
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const GlobalProvider = ({children}) => {
+    const [isLoggedIn, setisLoggedIn] = useState(false);
+    const [user, setuser] = useState(null);
+    const [isLoading, setisLoading] = useState(true);
+
+    useEffect(() => {
+        getCurrentUser()
+            .then((res)=>{
+                if(res){
+                    setisLoggedIn(true);
+                    setuser(res)
+                    console.log(user)
+                }else{
+                    setisLoggedIn(false)
+                    setuser(null)
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                setisLoading(false);
+            })
+    },[])
+
+    return(
+        <GlobalContext.Provider
+            value={{
+                isLoggedIn,
+                setisLoggedIn,
+                user,
+                setuser,
+                isLoading
+            }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export default GlobalProvider;
